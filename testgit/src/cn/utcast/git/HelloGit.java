d269ea307c095470c5b9ff7e287a9206f96ee31a
huahua <1197725829@qq.com>
2020-07-07 18:07:43 +0800

    use idea commit  version2

--- testgit/src/cn/utcast/git/HelloGit.java
+++ testgit/src/cn/utcast/git/HelloGit.java
@@ -3,5 +3,6 @@ package cn.utcast.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello this is my first version");
+        System.out.println("this is version 2");
     }
 }
